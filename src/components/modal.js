6cea36766e89e481a6111265552741988c663da7
minionstar <louislee214@gmail.com>
2023-09-28 10:30:56 -0700

    bug fix

--- src/components/modal.js
+++ src/components/modal.js
@@ -17,7 +17,7 @@ export default function PartnerModal(props) {
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto sm:max-w-3xl flex flex-row justify-center">
               {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-[#231a4f] outline-none focus:outline-none  w-[80%]">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col bg-[#231a4f] outline-none focus:outline-none  w-[80%]">
                 {/*header*/}
                 <div className="flex items-center justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                   <h3 className="text-3xl font-semibold">Tera Block</h3>
