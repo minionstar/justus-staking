a501c53fb0b49895fe3afacb149627f07a6c9f03
minionstar <louislee214@gmail.com>
2023-09-28 10:29:19 -0700

    bug fix

--- src/components/modal.js
+++ src/components/modal.js
@@ -15,7 +15,7 @@ export default function PartnerModal(props) {
       {showModal ? (
         <div className="bg-[#231a4f]">
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto sm:max-w-5xl flex flex-row justify-center">
+            <div className="relative w-auto my-6 mx-auto sm:max-w-3xl flex flex-row justify-center">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-[#231a4f] outline-none focus:outline-none  w-[80%]">
                 {/*header*/}
